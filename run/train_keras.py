2eda267d045e7c658360451c9be2406269b9864c
Junghwan John Goh <jhgoh@cern.ch>
2019-08-22 19:17:11 +0000

    split model from training script

--- run/train_keras.py
+++ run/train_keras.py
@@ -50,34 +50,11 @@ historyFile = os.path.join(args.outdir, 'history.csv')
 
 #from keras.utils.io_utils import HD5Matrix ## available from TF2.X
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Conv2D, LeakyReLU, BatchNormalization, MaxPooling2D, Dropout, Dense, Flatten
-from tensorflow.keras.models import Model
 
 ## Build model
-model = tf.keras.Sequential([
-    Conv2D(64, input_shape=shape[1:], kernel_size=(3,3), activation='relu', strides=1, padding='same'),
-    BatchNormalization(),
-    MaxPooling2D(pool_size=(2,2)),
-    Dropout(0.5),
-
-    Conv2D(128, kernel_size=(3, 3), activation='relu', strides=2, padding='same'),
-    BatchNormalization(),
-    MaxPooling2D(pool_size=(2, 2)),
-    Dropout(0.5),
-
-    Conv2D(256, kernel_size=(3, 3), activation='relu', strides=1, padding='same'),
-    BatchNormalization(),
-    MaxPooling2D(pool_size=(2, 2)),
-
-    Conv2D(256, kernel_size=(3, 3), activation='relu', strides=2, padding='same'),
-    BatchNormalization(),
-
-    Flatten(),
-    Dense(512, activation='relu'),
-    Dropout(0.5),
-
-    Dense(1, activation='sigmoid')
-])
+sys.path.append("../models")
+from keras.default import MyModel
+model = MyModel(shape[1:])
 
 optm = tf.keras.optimizers.Adam(0.001)
 
