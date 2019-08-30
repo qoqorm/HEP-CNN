8e52a2a37158462fcf5edc7094eae1fbb07cb5cc
Junghwan John Goh <jhgoh@cern.ch>
2019-08-30 16:54:15 +0900

    better plotting styles

--- run/drawPerfHistory.py
+++ run/drawPerfHistory.py
@@ -8,6 +8,11 @@ metric = 'CPU'
 
 dirs = [d for d in sys.argv[1:] if os.path.exists(d+'/batchHistory.csv')]
 cols = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple', 'tab:brown', 'tab:pink', 'tab:gray', 'tab:olive', 'tab:cyan']
+
+plt.rcParams['figure.figsize'] = (7, len(dirs)*1.2)
+plt.rcParams['lines.linewidth'] = 1
+plt.rcParams['lines.markersize'] = 5
+
 for i, d in enumerate(dirs):
     hostInfo, pars = d.split('/',1)
     hostAlias, hostSpec = hostInfo.replace('perf_', '').split('_',1)
@@ -23,10 +28,16 @@ for i, d in enumerate(dirs):
 
     ax = plt.subplot(len(dirs), 1, i+1)
     plt.plot(usage['time'], usage[metric], '.-', c=cols[i], label=(pars.replace('__', ' ')))
-    plt.xlabel('time')
+    plt.grid(linestyle=':')
+    if i == len(dirs)-1: plt.xlabel('time')
     plt.ylabel(metric)
     ax.set_xlim([0, 1000])
-    ax.set_ylim([0, 6000])
+    #plt.yscale('log')
+    if metric == 'CPU':
+        ax.set_ylim([0, 6000])
 
     plt.legend()
+plt.tight_layout()
+
+plt.savefig('%s.png' % (metric))
 plt.show()
