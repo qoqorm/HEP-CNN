b81da61f1acc1f3343cb501f52a5d32faaf6bab1
Junghwan John Goh <jhgoh@cern.ch>
2019-09-04 22:20:52 +0900

    overaly multiple plots

--- run/drawLossCurve.py
+++ run/drawLossCurve.py
@@ -3,27 +3,25 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import sys, os
 
-#plt.rcParams['figure.figsize'] = (7, len(dirs)*1.2)
-plt.rcParams['figure.figsize'] = (7, 2*2)
 plt.rcParams['lines.linewidth'] = 1
 plt.rcParams['lines.markersize'] = 5
 
+plt.rcParams['figure.figsize'] = (7, 2*2)
+ax1 = plt.subplot(2, 1, 1)
+ax2 = plt.subplot(2, 1, 2)
 for d in sys.argv[1:]:
     df = pd.read_csv(d)
 
-    ax = plt.subplot(2, 1, 1)
-    plt.plot(df['loss'], '.-', label='train')
-    plt.plot(df['val_loss'], '.-', label='val')
-    plt.ylabel('Loss')
+    ax1.plot(df['loss'], '.-', label='train')
+    ax1.plot(df['val_loss'], '.-', label='val')
+    #ax1.ylabel('Loss')
+    #ax1.yscale('log')
 
-    plt.yscale('log')
+    ax2.plot(df['acc'], '.-', label='train')
+    ax2.plot(df['val_acc'], '.-', label='val')
+    #ax.set_ylim([0,1])
+    #ax2.ylabel('Accuracy')
+    #ax2.xlabel('epoch')
 
-    ax = plt.subplot(2, 1, 2)
-    plt.plot(df['acc'], '.-', label='train')
-    plt.plot(df['val_acc'], '.-', label='val')
-    ax.set_ylim([0,1])
-    plt.ylabel('Accuracy')
-    plt.xlabel('epoch')
-
-    plt.tight_layout()
-    plt.show()
+plt.tight_layout()
+plt.show()
