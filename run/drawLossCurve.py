aa5798fedf50eefc38a95b914b4f9625f6235f97
Junghwan John Goh <jhgoh@cern.ch>
2019-09-04 22:34:16 +0900

    draw multple curves

--- run/drawLossCurve.py
+++ run/drawLossCurve.py
@@ -6,22 +6,24 @@ import sys, os
 plt.rcParams['lines.linewidth'] = 1
 plt.rcParams['lines.markersize'] = 5
 
-plt.rcParams['figure.figsize'] = (7, 2*2)
-ax1 = plt.subplot(2, 1, 1)
-ax2 = plt.subplot(2, 1, 2)
+plt.rcParams['figure.figsize'] = (7, 3*2)
+ax1 = plt.subplot(2, 2, 1, yscale='log', ylabel='Loss(train)')
+ax2 = plt.subplot(2, 2, 3, yscale='log', ylabel='Loss(val)')
+ax3 = plt.subplot(2, 2, 2, ylabel='Accuracy(train)')
+ax4 = plt.subplot(2, 2, 4, ylabel='Accuracy(val)', xlabel='epoch')
 for d in sys.argv[1:]:
     df = pd.read_csv(d)
 
-    ax1.plot(df['loss'], '.-', label='train')
-    ax1.plot(df['val_loss'], '.-', label='val')
-    #ax1.ylabel('Loss')
-    #ax1.yscale('log')
+    label = d.split('/')[-2]
 
-    ax2.plot(df['acc'], '.-', label='train')
-    ax2.plot(df['val_acc'], '.-', label='val')
+    ax1.plot(df['loss'], '.-', label=label)
+    ax2.plot(df['val_loss'], '.-', label=label)
+
+    ax3.plot(df['acc'], '.-', label=label)
+    ax4.plot(df['val_acc'], '.-', label=label)
     #ax.set_ylim([0,1])
-    #ax2.ylabel('Accuracy')
-    #ax2.xlabel('epoch')
+
+plt.legend()
 
 plt.tight_layout()
 plt.show()
