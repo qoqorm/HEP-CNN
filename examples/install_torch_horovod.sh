b04ab1dfc8c2776decdc7c69b0d49a4ea8bb1714
Junghwan John Goh <jhgoh@cern.ch>
2019-10-18 02:31:52 +0900

    make it properly working

--- examples/install_torch_horovod.sh
+++ examples/install_torch_horovod.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-module load gcc/7.2.0 openmpi
+module load gcc/7.2.0
+#module load openmpi
 
+source /apps/compiler/intel/19.0.4/impi/2019.4.243/intel64/bin/mpivars.sh relase_mt
 source /apps/applications/miniconda3/etc/profile.d/conda.sh
 conda create --prefix /scratch/$USER/conda/pytorch
 conda activate /scratch/$USER/conda/pytorch
