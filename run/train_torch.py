ba8bdbe4e478f71aeaf8aa041b9c31c15b2630c1
Junghwan John Goh <jhgoh@cern.ch>
2020-02-03 04:06:16 +0900

    rollback weight in BCELoss

--- run/train_torch.py
+++ run/train_torch.py
@@ -161,7 +161,7 @@ try:
             weight = weight.float().to(device)
 
             pred = model(data)
-            crit = torch.nn.BCELoss()#weight=weight)
+            crit = torch.nn.BCELoss(weight=weight)
             if device == 'cuda': crit = crit.cuda()
             l = crit(pred.view(-1), label)
             l.backward()
@@ -184,7 +184,7 @@ try:
             weight = weight.float().to(device)
 
             pred = model(data)
-            crit = torch.nn.BCELoss()#weight=weight)
+            crit = torch.nn.BCELoss(weight=weight)
             loss = crit(pred.view(-1), label)
 
             val_loss += loss.item()
