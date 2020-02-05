2c22122e433bfda3bd724324085659f667669396
Junghwan John Goh <jhgoh@cern.ch>
2020-02-06 00:53:11 +0900

    add circular padding in the pytorch way

--- run/train_torch.py
+++ run/train_torch.py
@@ -34,7 +34,8 @@ parser.add_argument('--noEarlyStopping', action='store_true', help='do not apply
 parser.add_argument('--batchPerStep', action='store', type=int, default=1, help='Number of batches per step (to emulate all-reduce)')
 parser.add_argument('--shuffle', action='store', type=bool, default=True, help='Shuffle batches for each epochs')
 parser.add_argument('--optimizer', action='store', choices=('sgd', 'adam', 'radam', 'ranger'), default='adam', help='optimizer to run')
-parser.add_argument('--model', action='store', choices=('default', 'log3ch', 'log5ch', 'original'), default='default', help='choice of model')
+parser.add_argument('--model', action='store', choices=('default', 'log3ch', 'log5ch', 'original', 'circpad', 'circpadlog3ch', 'circpadlog5ch'), 
+                               default='default', help='choice of model')
 
 args = parser.parse_args()
 
@@ -84,7 +85,7 @@ else:
 sysstat.update(annotation="read_trn")
 
 sysstat.update(annotation="open_val")
-if os.path.isdir(args.trndata):
+if os.path.isdir(args.valdata):
     valDataset = MySplitDataset(args.valdata, args.ntest, syslogger=sysstat)
 else:
     valDataset = MyDataset(args.valdata, args.ntest, syslogger=sysstat)
@@ -108,6 +109,8 @@ else:
 ## Build model
 if args.model == 'original':
     from HEPCNN.torch_model_original import MyModel
+elif 'circpad' in args.model:
+    from HEPCNN.torch_model_circpad import MyModel
 else:
     from HEPCNN.torch_model_default import MyModel
 model = MyModel(trnDataset.width, trnDataset.height, model=args.model)
