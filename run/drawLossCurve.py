a85d210231d05229e507e3c5c2dc59dfcdc4b4c6
Junghwan John Goh <jhgoh@cern.ch>
2020-03-14 11:44:51 +0900

    better plotting

--- run/drawLossCurve.py
+++ run/drawLossCurve.py
@@ -6,14 +6,22 @@ import sys, os
 plt.rcParams['lines.linewidth'] = 1
 plt.rcParams['lines.markersize'] = 5
 plt.rcParams["legend.loc"] = 'upper right'
+plt.rcParams["legend.frameon"] = False
+plt.rcParams["legend.loc"] = 'upper left'
 
-plt.rcParams['figure.figsize'] = (4*3, 3.5*2)
-ax1 = plt.subplot(2, 3, 1, yscale='log', ylabel='Loss(train)', xlabel='epoch')
-ax2 = plt.subplot(2, 3, 2, yscale='log', ylabel='Loss(val)', xlabel='epoch')
-#ax1 = plt.subplot(2, 3, 1, ylabel='Loss(train)')
-#ax2 = plt.subplot(2, 3, 2, ylabel='Loss(val)')
-ax3 = plt.subplot(2, 3, 4, ylabel='Accuracy(train)', xlabel='epoch')
-ax4 = plt.subplot(2, 3, 5, ylabel='Accuracy(val)', xlabel='epoch')
+plt.rcParams['figure.figsize'] = (4*2, 3.5*3)
+ax1 = plt.subplot(3, 2, 1, yscale='log', ylabel='Loss(train)', xlabel='epoch')
+ax2 = plt.subplot(3, 2, 2, yscale='log', ylabel='Loss(val)', xlabel='epoch')
+ax3 = plt.subplot(3, 2, 3, ylabel='Accuracy(train)', xlabel='epoch')
+ax4 = plt.subplot(3, 2, 4, ylabel='Accuracy(val)', xlabel='epoch')
+ax1.set_ylim([3e-2,2e-1])
+ax2.set_ylim([3e-2,2e-1])
+ax3.set_ylim([0.85,1])
+ax4.set_ylim([0.85,1])
+for ax in (ax1, ax2, ax3, ax4):
+    ax.grid(which='major', axis='both', linestyle='-.')
+    ax.grid(which='minor', linestyle=':')
+    ax.set_xlim([0,50])
 lines, labels = [], []
 for d in sys.argv[1:]:
     df = pd.read_csv(d)
@@ -25,14 +33,14 @@ for d in sys.argv[1:]:
 
     ax3.plot(df['acc'], '.-', label=label)
     ax4.plot(df['val_acc'], '.-', label=label)
-    #ax.set_ylim([0,1])
 
     lines.append(l[0])
     labels.append(label)
 
-ax5 = plt.subplot(1,3,3)
+ax5 = plt.subplot(3,1,3)
 ax5.legend(lines, labels)
 ax5.axis('off')
 
 plt.tight_layout()
 plt.show()
+
