93b12693dbffc033397700e900038e52393b0286
Junghwan John Goh <jhgoh@cern.ch>
2020-05-17 20:00:01 +0900

    evaluation for the new dataloader

--- run/eval_torch.py
+++ run/eval_torch.py
@@ -15,7 +15,6 @@ torch.set_num_threads(nthreads)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--batch', action='store', type=int, default=256, help='Batch size')
-parser.add_argument('-t', '--test', action='store', type=str, required=True, help='Test dataset')
 parser.add_argument('-d', '--input', action='store', type=str, required=True, help='directory with pretrained model parameters')
 parser.add_argument('--model', action='store', choices=('default', 'log3ch', 'log5ch', 'original', 'circpad', 'circpadlog3ch', 'circpadlog5ch'),
                                default='default', help='choice of model')
@@ -28,17 +27,34 @@ import pandas as pd
 if not os.path.exists(predFile):
     sys.path.append("../python")
 
-    from HEPCNN.torch_dataset_splited import HEPCNNSplitDataset as MySplitDataset
-    from HEPCNN.torch_dataset import HEPCNNDataset as MyDataset
+    print("Load data", end='')
+    from HEPCNN.dataset_hepcnn import HEPCNNDataset as MyDataset
 
-    kwargs = {'num_workers':min(4, nthreads), 'pin_memory':True}
+    myDataset = MyDataset()
+    myDataset.addSample("RPV_1400", "../data/CMS2018_unmerged/RPV/Gluino1400GeV/*/*.h5", weight=0.013/330599)
+    #myDataset.addSample("QCD_HT700to1000" , "../data/CMS2018_unmerged/QCD/HT700to1000/*/*.h5", weight=???)
+    #myDataset.addSample("QCD_HT1000to1500", "../data/CMS2018_unmerged/QCD/HT1000to1500/*/*.h5", weight=1094./15466225)
+    myDataset.addSample("QCD_HT1500to2000", "../data/CMS2018_unmerged/QCD/HT1500to2000/*/*.h5", weight=99.16/3199737)
+    myDataset.addSample("QCD_HT2000toInf" , "../data/CMS2018_unmerged/QCD/HT2000toInf/*/*.h5", weight=20.25/1520178)
+    myDataset.setProcessLabel("RPV_1400", 1)
+    myDataset.setProcessLabel("QCD_HT1500to2000", 0) ## This is not necessary because the default is 0
+    myDataset.setProcessLabel("QCD_HT2000toInf", 0) ## This is not necessary because the default is 0
+    myDataset.initialize(nWorkers=10)
+    print("done")
 
-    if os.path.isdir(args.test):
-        testDataset = MySplitDataset(args.test, nWorkers=nthreads//2)
-    else:
-        testDataset = MyDataset(args.test)
+    print("Split data", end='')
+    lengths = [int(0.6*len(myDataset)), int(0.2*len(myDataset))]
+    lengths.append(len(myDataset)-sum(lengths))
+    torch.manual_seed(123456)
+    trnDataset, valDataset, testDataset = torch.utils.data.random_split(myDataset, lengths)
+    torch.manual_seed(torch.initial_seed())
+    print("done")
+
+    kwargs = {'num_workers':min(4, nthreads)}#, 'pin_memory':True}
+
     testLoader = DataLoader(testDataset, batch_size=args.batch, shuffle=False, **kwargs)
 
+    print("Load model", end='')
     if os.path.exists(args.input+'/model.pkl'):
         print("Load saved model from", (args.input+'/model.pkl'))
         model = torch.load(args.input+'/model.pkl')
@@ -56,20 +72,25 @@ if not os.path.exists(predFile):
     if torch.cuda.is_available():
         model = model.cuda()
         device = 'cuda'
+    print('done')
+
+    model.load_state_dict(torch.load(args.input+'/weight_0.pkl'))
+    print('modify model', end='')
+    model.fc.add_module('output', torch.nn.Sigmoid())
+    model.eval()
+    print('done')
 
     from tqdm import tqdm
-    model.load_state_dict(torch.load(args.input+'/weight_0.pkl'))
-    model.eval()
-
-    labels, preds = [], []
-    for i, (data, label, weight) in enumerate(tqdm(testLoader)):
+    labels, weights, preds = [], [], []
+    for i, (data, label, weight, rescale) in enumerate(tqdm(testLoader)):
         data = data.float().to(device)
         weight = weight.float()
         pred = model(data).detach().to('cpu').float()
 
         labels.extend([x.item() for x in label])
         preds.extend([x.item() for x in pred.view(-1)])
-    df = pd.DataFrame({'label':labels, 'prediction':preds})
+        weights.extend([x.item() for x in (weight*rescale).view(-1)])
+    df = pd.DataFrame({'label':labels, 'weight':weights, 'prediction':preds})
     df.to_csv(predFile, index=False)
 
 from sklearn.metrics import roc_curve, roc_auc_score
@@ -78,10 +99,18 @@ tpr, fpr, thr = roc_curve(df['label'], df['prediction'], pos_label=0)
 auc = roc_auc_score(df['label'], df['prediction'])
 
 import matplotlib.pyplot as plt
-plt.plot(fpr, tpr, label='%s %.3f' % (args.input, auc))
+print(df.keys())
+df[df.label==0]['prediction'].hist(bins=100, alpha=0.7, color='red')
+df[df.label==1]['prediction'].hist(bins=100, alpha=0.7, color='blue')
+plt.yscale('log')
+plt.show()
+
+plt.plot(fpr, tpr, '.-', label='%s %.3f' % (args.input, auc))
 plt.xlabel('False Positive Rate')
 plt.ylabel('True Positive Rate')
-plt.xlim(0, 0.001)
+#plt.xlim(0, 0.001)
+plt.xlim(0, 1.000)
 plt.ylim(0, 1.000)
 plt.legend()
 plt.show()
+
