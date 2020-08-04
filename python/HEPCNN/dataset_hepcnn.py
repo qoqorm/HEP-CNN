5d472420be0a9f02db3c5e394b78bf5fe1bede5e
Junghwan John Goh <jhgoh@cern.ch>
2020-08-05 00:51:06 +0900

    sample management with pandas dataframe

--- python/HEPCNN/dataset_hepcnn.py
+++ python/HEPCNN/dataset_hepcnn.py
@@ -4,26 +4,22 @@ import torch
 from torch.utils.data import Dataset
 from bisect import bisect_right
 from glob import glob
+import pandas as pd
+import numpy as np
 
 class HEPCNNDataset(Dataset):
     def __init__(self, **kwargs):
         super(HEPCNNDataset, self).__init__()
         self.isLoaded = False
-        self.procFiles = {}
-        self.procLabels = {}
-
-        self.maxEventsList = [0,]
-        self.imagesList = []
-        self.labelsList = []
-        self.weightsList = []
-        self.rescaleList = []
+        self.fNames = []
+        self.sampleInfo = pd.DataFrame(columns=["procName", "fileName", "weight", "label", "fileIdx", "suffix"])
 
     def __getitem__(self, idx):
         if not self.isLoaded: self.load()
 
         fileIdx = bisect_right(self.maxEventsList, idx)-1
         offset = self.maxEventsList[fileIdx]
-        idx = idx - offset
+        idx = idx-int(offset)
 
         image  = self.imagesList[fileIdx][idx]
         label  = self.labelsList[fileIdx][idx]
@@ -33,75 +29,98 @@ class HEPCNNDataset(Dataset):
         return (image, label, weight, rescale)
 
     def __len__(self):
-        return self.maxEventsList[-1]
+        return int(self.maxEventsList[-1])
 
-    def addSample(self, procName, fileNames, weight=None, logger=None):
-        if logger: logger.update(annotation='Add sample %s <= %s' % (procName, fileNames))
+    def addSample(self, procName, fNamePattern, weight=None, logger=None):
+        if logger: logger.update(annotation='Add sample %s <= %s' % (procName, fileNamePattern))
         weightValue = weight ## Rename it just to be clear in the codes
 
-        if procName not in self.procFiles:
-            self.procFiles[procName] = [] ## this list keeps image index - later we will use this info to get total event and update weights, etc
+        for fName in glob(fNamePattern):
+            if not fName.endswith(".h5"): continue
+            fileIdx = len(self.fNames)
+            self.fNames.append(fName)
 
-        for fileName in glob(fileNames):
-            data = h5py.File(fileName, 'r', libver='latest', swmr=True)['all_events']
-            suffix = "_val" if 'images_val' in data else ""
-
-            images  = data['images'+suffix]
-            size = images.shape[0]
-
-            if weightValue is None: weights = data['weights'+suffix]
-            else: weights = torch.ones(size, dtype=torch.float32, requires_grad=False)*weightValue
-
-            nEventsInFile = len(weights)
-            self.maxEventsList.append(self.maxEventsList[-1]+nEventsInFile)
-
-            labels  = torch.zeros(size, dtype=torch.float32, requires_grad=False) ## Put dummy labels, to set later by calling setProcessLabel()
-            ## We will do this step for images later
-
-            fileIdx = len(self.imagesList)
-            self.procFiles[procName].append(fileIdx)
-            self.imagesList.append((fileName, ('images'+suffix)))
-            self.labelsList.append(labels)
-            self.weightsList.append(weights)
-            self.rescaleList.append(torch.ones(size, dtype=torch.float32, requires_grad=False))
+            info = {
+                'procName':procName, 'weight':weight, 'nEvents':0,
+                'label':0, ## default label, to be filled later
+                'fileName':fName, 'fileIdx':fileIdx,
+                'suffix':""
+            }
+            self.sampleInfo = self.sampleInfo.append(info, ignore_index=True)
 
     def setProcessLabel(self, procName, label):
-        for i in self.procFiles[procName]:
-            size = self.labelsList[i].shape[0]
-            self.labelsList[i] = torch.ones(size, dtype=torch.float32, requires_grad=False)*label
-            self.procLabels[procName] = label
+        self.sampleInfo.loc[self.sampleInfo.procName==procName, 'label'] = label
 
     def initialize(self, logger=None):
         if logger: logger.update(annotation='Reweights by category imbalance')
+
+        self.labelsList = []
+        self.weightsList = []
+        self.rescaleList = []
+        self.imagesList = []
+
+        nFiles = len(self.sampleInfo)
+        ## Load event contents
+        for i, fName in enumerate(self.sampleInfo['fileName']):
+            data = h5py.File(fName, 'r', libver='latest', swmr=True)['all_events']
+            suffix = "_val" if 'images_val' in data else ""
+
+            images  = data['images'+suffix]
+            nEvents = images.shape[0]
+            self.sampleInfo.loc[i, 'nEvents'] = nEvents
+            self.sampleInfo.loc[i, 'suffix'] = suffix
+
+            weightValue = self.sampleInfo.loc[i, 'weight']
+            if weightValue is None: weights = data['weights'+suffix]
+            else: weights = torch.ones(nEvents, dtype=torch.float32, requires_grad=False)*weightValue
+
+            ## set label and weight
+            label = self.sampleInfo['label'][i]
+            labels = torch.ones(nEvents, dtype=torch.int32, requires_grad=False)*label
+            self.labelsList.append(labels)
+            weight = self.sampleInfo['weight'][i]
+            weights = torch.ones(nEvents, dtype=torch.float32, requires_grad=False)*weight
+            self.weightsList.append(weights)
+            self.rescaleList.append(torch.ones(nEvents, dtype=torch.float32, requires_grad=False))
+
+        print(self.sampleInfo)
+
         ## Compute sum of weights for each label categories
         sumWByLabel = {}
         sumEByLabel = {}
-        for procName, fileIdxs in self.procFiles.items():
-            label = self.procLabels[procName]
-            if label not in sumWByLabel: sumWByLabel[label] = 0.
-            if label not in sumEByLabel: sumEByLabel[label] = 0.
-            procSumW = sum([sum(self.weightsList[i]) for i in fileIdxs])
-            procSumE = sum([len(self.weightsList[i]) for i in fileIdxs])
-            print("@@@ Process=%s nEvent=%d sumW=%.3f events/fb-1" % (procName, procSumE, procSumW.item()))
-            sumWByLabel[label] += procSumW
-            sumEByLabel[label] += procSumE
+        for label in self.sampleInfo['label']:
+            label = int(label)
+            sumWByLabel[label] = self.sampleInfo[self.sampleInfo.label==label]['weight'].sum()
+            sumEByLabel[label] = self.sampleInfo[self.sampleInfo.label==label]['nEvents'].sum()
+        ## Find overall rescale for the data imbalancing problem - fit to the category with maximum entries
+        maxSumELabel = max(sumEByLabel, key=lambda key: sumEByLabel[key])
+        maxWMaxSumELabel = self.sampleInfo[self.sampleInfo.label==maxSumELabel]['weight'].max()
 
+        ## Compute cumulative sums of nEvents, to be used for the file indexing
+        self.maxEventsList = np.concatenate(([0.], np.cumsum(self.sampleInfo['nEvents'])))
+
+        #### Find rescale factors - make weight to be 1 for each cat in the training step
         ## Find rescale factors - make average weight to be 1 for each cat in the training step
-        for procName, fileIdxs in self.procFiles.items():
-            label = self.procLabels[procName]
-            for i in fileIdxs: self.rescaleList[i] *= sumEByLabel[label]/sumWByLabel[label]
+        for fileIdx in self.sampleInfo['fileIdx']:
+            for l in self.sampleInfo.loc[self.sampleInfo.fileIdx==fileIdx, 'label']:
+                self.rescaleList[fileIdx] *= sumEByLabel[l]/sumWByLabel[l]
 
         ## Find overall rescale for the data imbalancing problem - fit to the category with maximum entries
         maxSumELabel = max(sumEByLabel, key=lambda key: sumEByLabel[key])
-        for procName, fileIdxs in self.procFiles.items():
-            label = self.procLabels[procName]
-            if label == maxSumELabel: continue
-            sf = sumEByLabel[maxSumELabel]/sumEByLabel[label]
-            print("@@@ Scale up the sample", label, "->", maxSumELabel, sf)
-            for i in fileIdxs: self.rescaleList[i] *= sf
-
-        data = h5py.File(self.imagesList[0][0], 'r', libver='latest', swmr=True)['all_events']
-        self.shape = data[self.imagesList[0][1]].shape[1:]
+        for fileIdx in self.sampleInfo['fileIdx']:
+            label = self.sampleInfo.loc[self.sampleInfo.fileIdx==fileIdx, 'label']
+            for l in label: ## this loop runs only once, by construction.
+                if l == maxSumELabel: break
+                #sf = sumWByLabel[maxSumELabel]/maxWMaxSumELabel/sumWByLabel[l]
+                sf = sumEByLabel[maxSumELabel]/sumEByLabel[l]
+                self.rescaleList[fileIdx] *= sf
+                #print("@@@ Scale sample label_%d(sumE=%g,sumW=%g)->label_%d, sf=%f" % (l, sumEByLabel[l], sumWByLabel[l], maxSumELabel, sf))
+        
+        ## Check the image format
+        fNameTemp = self.sampleInfo['fileName'][0]
+        suffixTemp = self.sampleInfo['suffix'][0]
+        data = h5py.File(fNameTemp, 'r', libver='latest', swmr=True)['all_events']
+        self.shape = data['images'+suffix].shape[1:]
         if self.shape[-1] <= 5: ## actual format was NHWC
             self.format = 'NHWC'
             self.height, self.width, self.channel = self.shape
@@ -111,6 +130,8 @@ class HEPCNNDataset(Dataset):
 
     def load(self):
         if self.isLoaded: return
-        for i, (fName, key) in enumerate(self.imagesList):
-            self.imagesList[i] = h5py.File(fName, 'r', libver='latest', swmr=True)['all_events/'+key]
+        for fName, suffix in zip(list(self.sampleInfo['fileName']), list(self.sampleInfo['suffix'])):
+            image = h5py.File(fName, 'r', libver='latest', swmr=True)['all_events/images'+suffix]
+            self.imagesList.append(image)
         self.isLoaded = True
+
