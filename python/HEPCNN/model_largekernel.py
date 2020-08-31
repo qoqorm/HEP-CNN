fix maxpool size, more fix on the pixel size formula

python/HEPCNN/model_largekernel.py
@@ -33,13 +33,13 @@ class MyModel(nn.Module):
             CircularPadX(14),
             nn.Conv2d(self.nch, 64, kernel_size=(14, 14), padding=(1,0)), ## padding=(height,width)
 
-            nn.MaxPool2d(kernel_size=(14, 14)),
+            nn.MaxPool2d(kernel_size=(2, 2)),
             nn.ReLU(),
             nn.BatchNorm2d(num_features=64, eps=0.001, momentum=0.99),
             nn.Dropout2d(0.5),
         ])
-        self.fh = (self.fh+14-14+1)//14
-        self.fw = (self.fw+2-14+1)//14
+        self.fh = (self.fh+2-14+1)//2
+        self.fw = (self.fw+2*14-14+1)//2
 
         self.conv.extend([
             CircularPadX(1),