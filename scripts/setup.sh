51b82a3797bb0f3dbde6487a901f4ab8e913df77
Junghwan John Goh <jhgoh@cern.ch>
2020-09-01 18:53:01 +0900

    update installation script and setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+source /apps/compiler/intel/19.0.5/impi/2019.5.281/intel64/bin/mpivars.sh release_mt
 source /apps/applications/miniconda3/etc/profile.d/conda.sh
-conda activate pytorch_v1.1.0
+module load git craype-mic-knl
+module load gcc/8.3.0
+export USE_CUDA=0
+export USE_MKLDNN=1
+export USE_OPENMP=1
+export USE_TBB=0
+
+conda activate /scratch/$USER/conda/nurion_torch
 
 export OMPI_MCA_btl_openib_allow_ib=1
 export OMPI_MCA_btl_openib_if_include="hfi1_0:1"
